e3a437c3824ea19b25173e475fb73363e2f75ca3
Carl Wilcoxon <62306383+CarlWilcoxon@users.noreply.github.com>
2024-10-02 05:28:50 -0500

    just wired POST contact button

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,37 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client'
-import App from './App/App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import Root, { 
+  loader as rootLoader,
+  action as rootAction,
+} from "./routes/root";
+import ErrorPage from './error-page';
 import './index.css'
+import Contact from './routes/contact';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      }    
+    ]
+  },
+]);
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
