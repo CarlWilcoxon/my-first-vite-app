1c2485381d10c230607bc74b7268e95e02c27032
Carl Wilcoxon <62306383+CarlWilcoxon@users.noreply.github.com>
2024-10-02 04:55:52 -0500

    quick save

---
+++ TODO.md
@@ -0,0 +1,5 @@
+# 
+[ ] make a todo list app
+[ ] solve a couple project euler problems?
+[ ] add a router
+[ ] add navbar
---
+++ src/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+
+          </li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
